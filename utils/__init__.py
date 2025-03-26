c249817d4953ef3fe75aa94b45e0377dae059c11
JDhruvR <dhruvramesh.joshi@iiitb.ac.in>
2025-03-26 23:44:10 +0530

    Added plot_roc_curve to init of utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,12 @@
 from .data_loader import load_electron_photon_dataset
 from .pre_processing import create_transform
-from .visualization import show_image, show_loss, show_accuracy
+from .visualization import show_image, show_loss, show_accuracy, plot_roc_curve
 
 __all__ = [
     'load_electron_photon_dataset',
     'create_transform',
     'show_image',
     'show_loss',
-    'show_accuracy'
+    'show_accuracy',
+    'plot_roc_curve'
 ]
